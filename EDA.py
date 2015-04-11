d1e1c6020a1f23821cb2a39e9487c0a180a511eb
Nitin Kashyap <ntinkashyap@gmail.com>
2015-04-11 00:28:14 -0500

    Decision Tree

--- EDA.py
+++ EDA.py
@@ -1,23 +1,23 @@
 __author__ = 'nitinkashyap'
-from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeClassifier as RFC
 import pandas as pd
 
 reader = pd.read_csv('train.csv')
-out = reader['hand'].tolist()
-print('Here')
+out = reader['hand'].values
 del reader['hand']
 values = reader.values
 test = pd.read_csv('test.csv')
 ids = test['id']
 del test['id']
 te_val = test.values
-k = SVR()
+k = RFC()
 X = k.fit(values, out)
 y = k.predict(te_val)
+print(max(y))
 final = pd.DataFrame()
 final['id'] = ids
 final['hand'] = y
-final.to_csv('Submission1.csv')
+final.to_csv('Submission1.csv', index=False)
 
 
 
